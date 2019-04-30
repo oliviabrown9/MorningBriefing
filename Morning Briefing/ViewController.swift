1aacee47373309fcd394ab6ad78c6fe69fb4bd4b
Olivia <obrown917@gmail.com>
2019-04-30 18:53:07 +0200

    Sample App

---
+++ Morning Briefing/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Morning Briefing
+//
+//  Created by Olivia Brown on 12/20/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
